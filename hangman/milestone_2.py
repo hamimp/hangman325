25c309ae0076316379b83ae4def6925588550353
hamim <hamim@hamim.com>
2023-12-05 04:02:53 +0000

    Task 4 completed

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -3,5 +3,11 @@ word_list = ["peach", "mango", "orange", "blackberry", "guava"]
 
 word = random.choice(word_list)
 
+
 guess = input("Plese enter a single letter: ")
-print(guess)
+if len(guess) == 1 and  guess.isalpha():
+    print( "Good guess!")
+    
+else:
+    print("Oops! That is not a valid input.")
+
